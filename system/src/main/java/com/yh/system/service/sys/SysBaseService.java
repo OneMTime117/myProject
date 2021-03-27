168ba34723de8536e4ba9cf7d28440e982c8cc01
OneMTime117 <756116832@qq.com>
2021-03-28 00:04:46 +0800

    update

---
+++ system/src/main/java/com/yh/system/service/sys/SysBaseService.java
@@ -0,0 +1,13 @@
+package com.yh.system.service.sys;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yh.system.domain.entity.sys.SysBase;
+
+public interface SysBaseService extends IService<SysBase> {
+
+
+}
+
+
+
+
---
+++ system/src/main/java/com/yh/system/service/sys/impl/SysBaseServiceImpl.java
@@ -0,0 +1,16 @@
+package com.yh.system.service.sys.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yh.system.domain.entity.sys.SysBase;
+import com.yh.system.mapper.sys.SysBaseMapper;
+import com.yh.system.service.sys.SysBaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SysBaseServiceImpl extends ServiceImpl<SysBaseMapper, SysBase> implements SysBaseService {
+
+}
+
+
+
+
